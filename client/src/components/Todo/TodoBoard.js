2dac30f927619964d4a5230026d1c56425bb5f65
ijwebber <isaacjwebber@gmail.com>
2020-05-23 10:17:21 +0100

    added svg image for the add button

--- client/src/components/Todo/TodoBoard.js
+++ client/src/components/Todo/TodoBoard.js
@@ -7,7 +7,13 @@ export default class TodoBoard extends Component {
             <div className="TodoBoard">
                 <div className="boardBack" />
                 <div className="board">
-
+                    <div id="addBar">
+                        <svg id="addButton" data-name="Component 4 – 1" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                            <line id="Line_4" data-name="Line 4" y2="25" transform="translate(12.5)" fill="none" stroke="#171717" stroke-width="3" />
+                            <line id="Line_5" data-name="Line 5" y2="25" transform="translate(25 12.5) rotate(90)" fill="none" stroke="#171717" stroke-width="3" />
+                        </svg>
+                    </div>
+                    <div id="content"></div>
                 </div>
             </div >
         );
