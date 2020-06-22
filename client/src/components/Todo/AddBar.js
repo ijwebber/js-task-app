4fa7ea00cb79ddf0d83ea889c5d4385af3f157e7
ijwebber <isaacjwebber@gmail.com>
2020-06-22 22:06:43 +0100

    tidied up

--- client/src/components/Todo/AddBar.js
+++ client/src/components/Todo/AddBar.js
@@ -3,10 +3,11 @@ import "./AddBar.css";
 
 export default class AddBar extends Component {
   // TODO try adding a function that calls the api seperately
+
   render() {
     return (
       <div id="addBar">
-        <form action="localhost:8000/todo" method="post">
+        <form method="post">
           <input type="text" id="newTodo" />
           <button id="addButton" type="submit">
             <span>Add</span>
